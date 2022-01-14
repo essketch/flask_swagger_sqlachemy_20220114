a7bdcc77367f8578bd465a2aed8d6c45af3eeb9d
김범준 <kejiegohome@kakao.com>
2022-01-14 16:43:15 +0900

    x

--- server/app.py
+++ server/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_restful_swagger_2 import Api
 
 def create_app(config_name):
     app = Flask(__name__)
 
     app.config.from_object(f'server.config.{config_name}')
+    api = Api(
+        app, api_spec_url='/api/spec', 
+        title='my_server_spec', 
+        api_server='0.1', 
+        catch_all_404s=True)
 
     return app
